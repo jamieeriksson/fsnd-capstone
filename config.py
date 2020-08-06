8670207fdd082e2e0d8640d0a9d1af08b1cfa306
Jamie Eriksson <jamieeriksson@gmail.com>
2020-08-05 19:15:21 -0700

    Add config file to git

---
+++ config.py
@@ -0,0 +1,35 @@
+from os import environ, path
+import os
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, ".env"))
+
+
+class Config:
+    SECRET_KEY = environ.get("SECRET_KEY")
+    SESSION_COOKIE_NAME = os.getenv("SESSION_COOKIE_NAME")
+    STATIC_FOLDER = "static"
+    TEMPLATES_FOLDER = "templates"
+
+    # Database
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProdConfig(Config):
+    ENV = "production"
+    DEBUG = False
+    TESTING = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevConfig(Config):
+    ENV = "development"
+    DEBUG = True
+    TESTING = True
+
